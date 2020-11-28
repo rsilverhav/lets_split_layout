cf59ea11e7179a2b5ffcc1375f8b54ae1fbde0d9
Robin <robinsilverhav@gmail.com>
2020-11-28 17:48:15 +0100

    changed colemak to numpad

--- keymap.c
+++ keymap.c
@@ -7,7 +7,7 @@
 // entirely and just use numbers.
 enum layer_names {
   _QWERTY = 0,
-  _COLEMAK = 1,
+  _NUMPAD = 1,
   _DVORAK = 2,
   _LOWER = 3,
   _RAISE = 4,
@@ -16,7 +16,7 @@ enum layer_names {
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  COLEMAK,
+  NUMPAD,
   DVORAK,
   LOWER,
   RAISE,
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  KC_LCTRL,  KC_PSCR, KC_LALT, KC_LGUI, LOWER,   KC_BSPC,  KC_SPC,  RAISE,   KC_VOLD, KC_VOLU, KC_MPLY, KC_MNXT  \
+  KC_LCTRL, RAISE, KC_LALT, KC_LGUI, LOWER,   KC_BSPC,  KC_SPC,  RAISE,   KC_VOLD, KC_VOLU, KC_MPLY, ADJUST  \
 ),
 
 /* Colemak
@@ -61,11 +61,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |Adjust| PrtSc| GUI  | Alt  |Lower | Tab  |Space |Raise | VolDn| VolUp| Pl/Ps| Next |
  * `-----------------------------------------------------------------------------------'
  */
-[_COLEMAK] = LAYOUT_ortho_4x12( \
-  KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC, \
-  KC_LCTL,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  ADJUST,  KC_PSCR, KC_LGUI, KC_LALT, LOWER,   KC_TAB,  KC_SPC,  RAISE,   KC_VOLD, KC_VOLU, KC_MPLY, KC_MNXT  \
+[_NUMPAD] = LAYOUT_ortho_4x12( \
+  KC_ESC,  KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_MINUS,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC, \
+  KC_LCTL, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_PLUS,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
+  KC_LSFT, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_SLASH,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+  ADJUST,  KC_PSCR, KC_KP_2, KC_LALT, KC_KP_ASTERISK,   KC_TAB,  KC_SPC,  RAISE,   KC_VOLD, KC_VOLU, KC_MPLY, KC_MNXT  \
 ),
 
 /* Dvorak
@@ -135,10 +135,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] =  LAYOUT_ortho_4x12( \
-  _______, RESET,   _______,  QWERTY,  COLEMAK, DVORAK,  _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, KC_DEL,  \
-  _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, \
+  _______, KC_MPRV, KC_MPLY, KC_MNXT,  QWERTY,  NUMPAD, _______,  _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, \
+  _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+  _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 )
 
 
@@ -152,9 +152,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case COLEMAK:
+    case NUMPAD:
       if (record->event.pressed) {
-        set_single_persistent_default_layer(_COLEMAK);
+        set_single_persistent_default_layer(_NUMPAD);
       }
       return false;
       break;
